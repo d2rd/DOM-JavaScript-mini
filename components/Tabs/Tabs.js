dcff5a72fc7ab5d0de6784bc259de8a00164f257
Glenn-David Daniel <d2rd@outlook.com>
2018-01-24 14:25:43 -0800

    WIP progress save

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,15 +1,15 @@
 
 class TabItem {
   constructor(element) {
-    // attach dom element to object. Example in Tabs class
+    this.element = element;  // attach dom element to object. Example in Tabs class
   }
 
   select() {
-    // should use classList
+    this.element.classList.add("Tabs__item-selected");  // should use classList
   }
 
   deselect() {
-    // should use classList
+    this.element.classList.remove("Tabs__item-selected");  // should use classList
   }
 }
 
@@ -26,19 +26,19 @@ class TabLink {
   };
 
   select() {
-    // select this link
-    // select the associated tab
+    this.element.classList.add("Tabs__link-selected"); // select this link
+    this.tabItem.select(); // select the associated tab
   }
 
   deselect() {
-    // deselect this link
-    // deselect the associated tab
+    this.element.classList.remove("Tabs__link-selected"); // deselect this link
+    this.tabItem.deselect(); // deselect the associated tab
   }
 }
 
 class Tabs {
   constructor(element) {
-    this.element = element;// attaches the dom node to the object as "this.element"
+    this.element = element; // attaches the dom node to the object as "this.element"
     this.links = element.querySelectorAll(".Tabs__link");
     this.links = Array.from(this.links).map((link) => {
       return new TabLink(link, this);
@@ -48,7 +48,7 @@ class Tabs {
   }
 
   init() {
-    // select the first link and tab upon ititialization
+    // select the first link and tab upon initialization
   }
 
   updateActive(newActive) {
@@ -63,4 +63,5 @@ class Tabs {
 }
 
 let tabs = document.querySelectorAll(".Tabs");
+console.log("tabs after querySelectorAll", tabs);  // Debug tip: make a note and look at it in the DOM
 tabs = Array.from(tabs).map(tabs => new Tabs(tabs));
